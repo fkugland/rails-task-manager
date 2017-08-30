c7780b4a5f8b63068e25db0bde2a849009ca9a88
Frederike Kugland <frederike.kugland@whu.edu>
2017-08-31 01:24:50 +0200

    updating

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(
+  name: "Wash the dishes",
+  status: false
+)
+
+Task.create(
+  name: "Buy some icecream",
+  status: true
+)
+
+Task.create(
+  name: "Wonder about life",
+  status: true
+)
